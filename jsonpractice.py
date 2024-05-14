6919c801d798986fea6f91dc9335f2f95b0d6eb4
Yohana8 <Ygebregziabher@sea.yearup.org>
2024-05-14 09:44:02 -0700

    First Repo Push

---
+++ jsonpractice.py
@@ -0,0 +1,12 @@
+
+import json 
+
+#Create the data dictionary
+data = {
+    'name':'John Doe',
+    'age': 30,
+    'city':'New York,NY',
+    'interest':['Golf','Running','Football','Traveling'],
+    'is_student':False
+
+}
